136ddf142907be34679b19e5dacb2e3bafa8200c
Federico Rossendy <fede07@users.noreply.github.com>
2023-10-16 16:23:30 -0300

    Start

---
+++ main.py
@@ -0,0 +1,19 @@
+from tkinter import Tk, Button, Label, Canvas, PhotoImage
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(width=600, height=600, padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+
+canvas.grid(column=1, row=1)
+
+window.mainloop()
